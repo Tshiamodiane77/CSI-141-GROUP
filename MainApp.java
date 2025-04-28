1b2c8ca0043b14ee296687c017d296483a7b6fa2
GaoneBanno <bannogaone@gmail.com>
2025-04-28 19:17:00 +0200

    Add files via upload

---
+++ MainApp.java
@@ -0,0 +1,21 @@
+public class MainApp 
+{
+    public static void main(String[] args)
+     {
+        try {
+            // Create sample products
+            Product[] products = new Product[5];
+            products[0] = new Product("Flour", 150.00, 12);
+            products[1] = new Product("Rice", 200.00, 10);
+            products[2] = new Product("Macaroni", 45.50, 3);
+            products[3] = new Product("Maize meal", 100.99, 13);
+            products[4] = new Product("Braii pack", 198.90, 5);
+
+            // Create a customer
+            Customer customer = new Customer("Ame Botshelo", "amebotshelo@gmail.com");
+
+            // Display customer information
+            System.out.println("Customer: " + customer.getName() + ", Email: " + customer.getEmail());
+        }
+    }
+}
